38af0b9cf335be295d652d5f6d1aad759e55a9ea
Jibon <manvirjibon@gmail.com>
2023-12-17 18:06:10 +0600

    flex wrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import b1 from "./assets/b1.jpg"
 
 function App() {
 
@@ -9,10 +9,21 @@ function App() {
       <div className=" flex justify-center gap-x-24 mt-16">
        <h2 className="bg-red-500 px-6 rounded-lg py-1 text-white hover:bg-red-700 duration-500">Red</h2>
        <h2 className="bg-black px-6 rounded-lg py-1 text-white hover:bg-[#434141] duration-500">Black</h2>
-       <h2>Blue</h2>
-       <h2>Red</h2>
-       <h2>Yellow</h2>
+       <h2 className="bg-blue-500 px-6 rounded-lg py-1 text-white hover:bg-blue-700 duration-500">Blue</h2>
+       <h2 className="bg-yellow-500 px-6 rounded-lg py-1 text-white hover:bg-yellow-700 duration-500">Yellow</h2>
       </div>
+
+     <div className=" flex flex-wrap">
+     <div className="w-[300px]">
+  <div>
+  <img src={b1} alt={b1} className=""/>
+   <div className=" flex justify-between">
+   <p>Colour: Blue</p>
+   <p>Price:2Core</p>
+   </div>
+  </div>
+      </div>
+     </div>
       </section>     
     </>
   )
