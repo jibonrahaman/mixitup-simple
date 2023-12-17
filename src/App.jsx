783105c057d7c2f85d208f3c0f2344717c61aa77
Jibon <manvirjibon@gmail.com>
2023-12-17 19:22:36 +0600

    dual map

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,14 @@ const [task,setTask]=useState([])
        <h2 className="bg-black px-6 rounded-lg py-1 text-white hover:bg-[#434141] duration-500" onClick={()=>handleCar("Black")}>Black</h2>
        <h2 className="bg-blue-500 px-6 rounded-lg py-1 text-white hover:bg-blue-700 duration-500" onClick={()=>handleCar("Blue")}>Blue</h2>
        <h2 className="bg-yellow-500 px-6 rounded-lg py-1 text-white hover:bg-yellow-700 duration-500" onClick={()=>handleCar("Yellow")}>Yellow</h2>
+        <h2 className="bg-yellow-500 px-6 rounded-lg py-1 text-white hover:bg-yellow-700 duration-500" onClick={()=>handleCar("Yellow")}>Yellow</h2>
       </div>
 
 
       <div className=" flex flex-wrap gap-x-9 ml-20 mt-8">
-    { task.length>0 
-      showCar.map((item,index)=>{
+    { task.length>0 ?
+
+      task.map((item,index)=>{
         const {img,price}=item
        return <div key={index} className="w-[300px] my-4">
         <img src={img} alt={img} className=" w-full h-[200px]"/>
@@ -34,6 +36,17 @@ const [task,setTask]=useState([])
         <p>Price:{price}</p>
         </div>
            </div>       })
+           :
+
+           showCar.map((item,index)=>{
+            const {img,price}=item
+           return <div key={index} className="w-[300px] my-4">
+            <img src={img} alt={img} className=" w-full h-[200px]"/>
+            <div className=" flex justify-between">
+            {/* <p>Colour: {colour}</p> */}
+            <p>Price:{price}</p>
+            </div>
+               </div>       })
     }
  
       </div>
