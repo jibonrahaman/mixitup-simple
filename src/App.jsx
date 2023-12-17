f937d5025e07a0502a4ae0cdcd8cca4e8d681c57
Jibon <manvirjibon@gmail.com>
2023-12-17 18:00:17 +0600

    design

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ function App() {
   return (
     <>
      <section>
-      <div className=" flex">
+      <div className=" flex justify-center gap-x-24 mt-16">
+       <h2 className="bg-red-500 px-6 rounded-lg py-1 text-white hover:bg-red-700 duration-500">Red</h2>
+       <h2 className="bg-black px-6 rounded-lg py-1 text-white hover:bg-[#434141] duration-500">Black</h2>
+       <h2>Blue</h2>
        <h2>Red</h2>
-       <h2>Red</h2>
-       <h2>Red</h2>
-       <h2>Red</h2>
-       <h2>Red</h2>
+       <h2>Yellow</h2>
       </div>
       </section>     
     </>
